55aac01cf4dc0153510e6f3eca3278b171148819
dddimcha <dddimcha@gmail.com>
2025-07-31 11:16:51 +0200

    Implement ARM64 architecture support
    - Add complete ARM64 CPU functions (interrupts, timestamp, info)
    - Implement PL011 UART driver for console output
    - Add missing kernel symbols to linker script
    - Fix undefined references in ARM64 build

--- kernel/arch/aarch64/cpu.c
+++ kernel/arch/aarch64/cpu.c
@@ -1,11 +1,27 @@
 /* EMBODIOS ARM64 CPU functions */
 
 #include "embodios/types.h"
+#include "embodios/cpu.h"
+
+static struct cpu_info arm64_cpu_info = {
+    .vendor = "ARM",
+    .model = "Generic ARMv8",
+    .family = 8,
+    .model_id = 0,
+    .stepping = 0,
+    .features = 0,
+    .cores = 1
+};
 
 void cpu_init(void)
 {
     /* ARM64 CPU initialization stub */
     /* TODO: Initialize CPU features, caches, etc. */
+}
+
+void arch_cpu_init(void)
+{
+    cpu_init();
 }
 
 void cpu_halt(void)
@@ -16,8 +32,47 @@ void cpu_halt(void)
     }
 }
 
-uint64_t cpu_get_features(void)
+void arch_halt(void)
+{
+    cpu_halt();
+}
+
+uint32_t cpu_get_features(void)
 {
     /* TODO: Read CPU feature registers */
     return 0;
+}
+
+uint64_t cpu_get_timestamp(void)
+{
+    /* ARM64: Read system counter */
+    uint64_t val;
+    __asm__ volatile("mrs %0, cntvct_el0" : "=r"(val));
+    return val;
+}
+
+struct cpu_info* cpu_get_info(void)
+{
+    return &arm64_cpu_info;
+}
+
+void arch_enable_interrupts(void)
+{
+    /* Enable interrupts */
+    __asm__ volatile("msr daifclr, #2");
+}
+
+void arch_disable_interrupts(void)
+{
+    /* Disable interrupts */
+    __asm__ volatile("msr daifset, #2");
+}
+
+void arch_reboot(void)
+{
+    /* ARM64 reboot - system-specific */
+    /* TODO: Implement proper reboot sequence */
+    while (1) {
+        __asm__ volatile("wfe");
+    }
 }
